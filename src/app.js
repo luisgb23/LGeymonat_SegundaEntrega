f1a41ebac68f5f2d19d97c192375bf4d9ec68cfc
luisgb23 <luisgeymonatbidart@gmail.com>
2025-05-13 16:51:26 -0300

    ajustes realizados

--- src/app.js
+++ src/app.js
@@ -21,7 +21,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/src/public')));
 
-// Handlebars
 app.engine('handlebars', handlebars.engine());
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, '/src/views'));
@@ -33,16 +32,12 @@ app.use('/', viewsRouter(productManager));
 
 console.log('Vistas buscadas en:', path.join(__dirname, '/src/views'));
 
-
-// Socket.io
 io.on('connection', async socket => {
   console.log('Cliente conectado por socket');
 
-  // Enviar productos iniciales
   const productos = await productManager.getProducts();
   socket.emit('updateProducts', productos);
 
-  // Agregado desde el frontend por formulario
   socket.on('addProduct', async product => {
     await productManager.addProduct(product);
     const productosActualizados = await productManager.getProducts();
--- src/public/js/index.js
+++ src/public/js/index.js
@@ -33,12 +33,35 @@ socket.on('updateProducts', products => {
     `;
     tableBody.appendChild(row);
   });
-
+/*
   document.querySelectorAll('.btn-danger').forEach(btn => {
     btn.addEventListener('click', () => {
       const id = parseInt(btn.getAttribute('data-id'));
       socket.emit('deleteProduct', id);
     });
   });
+  */
+  document.querySelectorAll('.btn-danger').forEach(button => {
+    button.addEventListener('click', () => {
+      const id = parseInt(button.getAttribute('data-id'));
+
+      Swal.fire({
+        title: '¿Estás seguro?',
+        text: `¿Querés eliminar el producto con ID ${id}?`,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#3085d6',
+        confirmButtonText: 'Sí, eliminar',
+        cancelButtonText: 'Cancelar'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          socket.emit('deleteProduct', id);
+          Swal.fire('Eliminado', 'El producto ha sido eliminado.', 'success');
+        }
+      });
+    });
+  });
 });
+
 
